628f7066b97418765980030a3a263b675ffc9324
//Dennis <106703217+Dennis-ty@users.noreply.github.com>
2024-03-26 16:14:08 -0700

    Add file header comments to LinkedFrontBackCappedList.java

--- LinkedFrontBackCappedList.java
+++ LinkedFrontBackCappedList.java
@@ -1,3 +1,17 @@
+/* @course: CS 111C Data Structures and Algorithms: Java 
+ * @Section: 933 
+ * CRN: 35087
+ * @instructor: Jessica Masters
+ * @authors(A to Z): Dennis Sun (ysun52@mail.ccsf.edu) ; 
+ *                   Junru Zhou (jzhou223@mail.ccsf.edu);
+ * 					 Keke Ning (kning2@mail.ccsf.edu);
+ * @members: 3
+ * @project B
+ * @date: 03/26/2024
+ * @file: LinkedFrontBackCappedList.java
+ * @purpose: Implements the interface, the class uses linked nodes to implement the list.
+ * @version 0.1
+*/
 public class LinkedFrontBackCappedList<T> implements FrontBackCappedList<T> {
 
 	private Node head, tail;
